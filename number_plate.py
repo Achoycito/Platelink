5291f97ac4a851485c85db6336a8dd35de448f7b
Alan Gonzalez <alanglez360@outlook.es>
2023-10-29 22:43:42 -0700

    Mejora para capturar matricula completa; Test fallido de guardar imagen

--- number_plate.py
+++ number_plate.py
@@ -1,10 +1,15 @@
 import cv2
+import math
 
 #harcascade = "model/haarcascade_russian_plate_number.xml"
 
 # harcascade = "model/bcs_plate_cascade.xml"
+# harcascade = "model/bcs_blue.xml"
+# harcascade = "model/green.xml"
+harcascade = "model/cascade24min.xml"
+
 # harcascade = "model/bcs_cascade_2.xml"
-harcascade = "model/cascade.xml"
+# harcascade = "model/cascade.xml"
 
 cap = cv2.VideoCapture(0)
 
@@ -12,6 +17,7 @@ cap.set(3, 640) #Ancho
 cap.set(4, 480) #Alto
 
 min_area = 10000 #Area minima de la imagen de la matricula
+count = 0
 
 while True:
     success, img = cap.read()
@@ -19,20 +25,44 @@ while True:
     plate_cascade = cv2.CascadeClassifier(harcascade) #Detectar con el haar
     img_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Colorear imagen en escala de grises
 
-    plates = plate_cascade.detectMultiScale(img_grey, 1.1, 4)
+    plates = plate_cascade.detectMultiScale(img, 1.1, 4)
 
     for(x, y, w, h) in plates:
-        area = w*h
+        wco = math.floor(w*0.1)
+        hco = math.floor(h*0.1)
+
+        area = (w + wco*2) * (h+ hco*2)
 
         if area > min_area: #Creo que aquí está checando que lo que detectó sea de un tamaño minimo para ver si se trata de una placa o nel
-            cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 0), 2) #Crea un rectangulo en la imagen que capturo de la matricula (x, y, ancho, alto, color, grosor)
+            # cv2.rectangle(img, (x,y), (x+w, y+h), (0, 255, 255), 2) #Crea un rectangulo en la imagen que capturo de la matricula (x, y, ancho, alto, color, grosor)
+            cv2.rectangle(img, (x-hco,y-hco), (x+w+wco, y+h+hco), (0, 255, 0), 2) #Crea un rectangulo en la imagen que capturo de la matricula (x, y, ancho, alto, color, grosor)
             cv2.putText(img, "MATRICULA", (x,y-5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 255), 2) #Escribir texto "Num Placa" (pos, fuente, tamaño, color, grosor)
 
+            yCoord = y-hco if y-hco>0 else y
+            xCoord = x-wco if x-wco>0 else x
+
             #Ventanita con el puro resultado
-            img_roi = img[y: y+h, x:x+w]
+            img_roi = img[yCoord: y+h+hco, xCoord:x+w+wco]
+            cv2.imshow("Extracted", img_roi)
 
 
     cv2.imshow("Result", img) #Ventanita
 
+
+    # if cv2.waitKey(1) & 0xFF == ord('s'):
+    #     cv2.imwrite("plates\\scaned_img_" + str(count) + ".jpg", img_roi)
+    #     cv2.rectangle(img, (0,200), (640,300), (0,255,0), cv2.FILLED)
+    #     cv2.putText(img, "Plate Saved", (150, 265), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (0, 0, 255), 2)
+    #     cv2.imshow("Results",img)
+    #     cv2.waitKey(500)
+    #     count += 1
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+
+    # if cv2.waitKey(1) & 0xFF == ord('s'):
+    #     print("GUARDAR IMAGEN")
+    #     cv2.imwrite("capturas/img_"+str(count)+".jpg", img_roi)
+    #     cv2.waitKey(500)
+    #     count+=1
